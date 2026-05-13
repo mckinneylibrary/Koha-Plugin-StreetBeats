b52a0047f314f49975b5657b7947992aa1cfc328
mckinneylibrary <56939901+mckinneylibrary@users.noreply.github.com>
2026-05-13 09:30:38 -0500

    Create streetbeats_slots table in install.sql

--- install.sql
+++ install.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS streetbeats_slots (
+    slot_id INT(11) NOT NULL AUTO_INCREMENT,
+    borrowernumber INT(11) NOT NULL,
+    location_id INT(11) NOT NULL,
+    start_time DATETIME NOT NULL,
+    PRIMARY KEY (slot_id),
+    CONSTRAINT fk_sb_patron FOREIGN KEY (borrowernumber) REFERENCES borrowers (borrowernumber)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
