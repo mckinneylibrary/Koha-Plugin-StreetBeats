8bbe0445b290c85cc4b1db98d00b6dd1f224b48b
mckinneylibrary <56939901+mckinneylibrary@users.noreply.github.com>
2026-05-13 09:29:35 -0500

    Add StreetBeats integration plugin
    Implement StreetBeats plugin for musician booking.

--- Koha/Plugin/Com/McKinney/StreatBeats.pm
+++ Koha/Plugin/Com/McKinney/StreatBeats.pm
@@ -1,1 +1,32 @@
+package Koha::Plugin::Com::McKinney::StreetBeats;
 
+use Modern::Perl;
+use base qw(Koha::Plugins::Base);
+use C4::Context;
+
+sub new {
+    my ( $class, $args ) = @_;
+    $args->{'metadata'} = {
+        name            => 'StreetBeats Integration',
+        author          => 'McKinney Public Library',
+        description     => 'Centralized musician booking via library cards.',
+        date_authored   => '2026-05-13',
+        date_updated    => '2026-05-13',
+        minimum_version => '22.11',
+        version         => '1.0',
+    };
+    return $class->SUPER::new($args);
+}
+
+sub install {
+    my ( $self, $args ) = @_;
+    my $db = C4::Context->dbh;
+    # Execute your MariaDB table creation logic here
+    return 1;
+}
+
+sub tool {
+    my ( $self, $args ) = @_;
+    my $template = $self->get_template({ file => 'streetbeats/report.tt' });
+    print $template->output();
+}
